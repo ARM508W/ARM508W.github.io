2af2faa1c93c765af630eaaf42b9d9bf6acdd1a0
ARM508W <martinez.armando508w@gmail.com>
2025-02-24 00:06:00 -0700

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,24 @@
+export const SITE_TITLE = "ARMW";
+export const SITE_DESCRIPTION =
+  `Website to document past and current projects at LHS and the future`.trim();
+
+export const PAST_PROJECTS =
+  `Tech 1: Weather Paper Lantern, Tech 2: Capacitive Touch Simon Says, Tech 3: Alterna, Summer 2024: TOTY Custom Gameboy Color ,Capstone Project: Encore`.split(
+    ",",
+  );
+export const ABOUT =
+  `The engineering notebook website. This is all the projects I have worked on and info relating to them.`.trim();
+export const QUOTE = "I AM STUCK IN ENGINEERING ROOM!!!";
+export const QUOTEs = ["I am stuck in the engineering room.", "No questions right now", "Check behind the award"]
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Blog",
+  },
+  {
+    title: "Github",
+    href: "//github.com/ARM508W",
+  },
+  //{
+    //title: "Source",
+  //},
+];
---
+++ src/content/blog/capstone-design-creation-cafe.md
@@ -0,0 +1,15 @@
+---
+title: "Capstone Project: Pico Multiplexer PCB"
+description: "The process of designing the custom PCB for the Pico W"
+pubDate: "Feb 20 2025"
+updatedDate: "Feb 21 2025"
+heroImage: "/pico-pcb-ascii.png"
+visibility: "public"
+---
+
+My process and reasoning for designing a custom PCB
+
+## Reasoning
+For my capstone project, I would need to be able to detect whenever an object was taken off the desk display. The method I ended up settling on was using magnets for the detecting method. 
+
+# Test Blog
